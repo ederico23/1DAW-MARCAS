5b7dcda3265ecc878d7584f06bd49d594aad843e
unknown <Eder Gracia>
2026-01-29 13:57:43 +0100

    practica1 tema5 hecha

--- JS/tarea6-7.js
+++ JS/tarea6-7.js
@@ -1,1 +1,14 @@
+var contador = 1;
+
+while (contador<=10) {
+    if (contador%2 == 0) {
+        document.writeln(
+            "es par"
+        )
+    }
+    document.writeln(
+        contador + "<br>"
+    )
+    contador++;
+}
 
---
+++ JS/tarea8.js
@@ -0,0 +1,8 @@
+var numU;
+numU = Number(prompt("escribe un numero"));
+
+while(numU != 0){
+    document.writeln(
+        numU-- + "<br>"
+    )
+}
